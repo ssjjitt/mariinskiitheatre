bf2567f6c81d27e89dbd387195116fb995533ba6
ssjjitt <120317828+ssjjitt@users.noreply.github.com>
2023-05-05 13:07:58 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,40 @@
+// для плавного появления блоков
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+
+// для стрелки
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
+
+// для формы
+ var modal = document.getElementById("id01");
+ window.onclick = function (event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ };
---
+++ script/secondvar.js
@@ -0,0 +1,22 @@
+$("#vklogo")
+  .mouseover(function () {
+    $(this).attr("src","images/vklogo2.png");
+  })
+  .mouseout(function () {
+    $(this).attr("src","images/vklogo.png");
+  });
+  $("#oklogo")
+  .mouseover(function () {
+    $(this).attr("src","images/oklogo2.png");
+  })
+  .mouseout(function () {
+    $(this).attr("src","images/oklogo.png");
+  });
+  $("#tglogo")
+  .mouseover(function () {
+    $(this).attr("src","images/tglogo2.png");
+  })
+  .mouseout(function () {
+    $(this).attr("src","images/tglogo.png");
+  });
+
